0fd0d56a5125376afe4a15056af72b49da5b0839
AKSH DESAI <68705063+AKSHDESAI1@users.noreply.github.com>
2022-02-05 18:21:18 +0530

    20CE020 Python Assignment

--- Practical_3/Find_Captain_Room_Number.py
+++ Practical_3/Find_Captain_Room_Number.py
@@ -7,7 +7,7 @@
 
 # Name: Aksh K Desai
 # Id: 20CE020
-# Github Repository Link: https://github.com/AKSHDESAI1/CE259_Practical-2_Assignment.git
+# Github Repository Link: https://github.com/AKSHDESAI1/CE259_Python_Practicals.git
 
 user_input = int(input())
 lis1 = list(map(int, input().split(' ')))
