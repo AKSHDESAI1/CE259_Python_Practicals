f4c2b407ac579b53e1974f78b0a3934e1d1a16ec
AKSH DESAI <68705063+AKSHDESAI1@users.noreply.github.com>
2022-02-05 18:21:46 +0530

    20CE020 Python Assignment

--- Practical_4/Find_runner-up_from_given_list.py
+++ Practical_4/Find_runner-up_from_given_list.py
@@ -1,7 +1,7 @@
 # Aim: Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
 # Name: Aksh k Desai
 # Id: 20CE020
-# Github Repository Link: https://github.com/AKSHDESAI1/CE259_Practical-2_Assignment.git
+# Github Repository Link: https://github.com/AKSHDESAI1/CE259_Python_Practicals.git
 
 user_input = int(input())
 lis1 = list(map(int, input().split(' ')))
