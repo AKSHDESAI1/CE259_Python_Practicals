30a042e35a60e087f91ad00b0346ff6cb9158984
AKSH DESAI <68705063+AKSHDESAI1@users.noreply.github.com>
2022-04-19 20:57:07 +0530

    Update Practical_2.py

--- Programmin Assignments/Practical_2.py
+++ Programmin Assignments/Practical_2.py
@@ -1,1 +1,17 @@
-hi
+# Write a procedure to find min, max, mean, standard deviation, variance  of  number list. Example Input :  10  50  80  70  49  23  11   4output :  4  80  37. 13  27. 25  848. 70
+
+# Name: Aksh k Desai
+# Id: 20CE020
+# Github Repository Link: https://github.com/AKSHDESAI1/CE259_Python_Practicals.git
+
+import statistics
+
+number = list(map(int,input().split()))
+
+max_number = max(number)
+min_number = min(number)
+mean_number = round(statistics.mean(number),2)
+standard_deviation = round(statistics.stdev(number),2)
+variance_number = round(statistics.variance(number),2)
+
+print(max_number, min_number, mean_number, standard_deviation, variance_number)
